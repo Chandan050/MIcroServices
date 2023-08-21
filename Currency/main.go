d4c5eb99d706f753c6625af581800bbb512a207b
Chandan050 <chandanreddybv99@gmail.com>
2023-08-21 10:12:35 +0530

    firstCommit

---
+++ Currency/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"golangProjects/Microservice/Currency/protos"
+	"golangProjects/Microservice/Currency/server"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// 1. Create a new client with the default settings:
+	log1 := log.Default()
+	protos
+
+	gs := grpc.NewServer()
+	cs := server.NewCurrency(log1)
+
+	protos.RegisterCurrencyServer(gs, cs)
+	l, err := net.Listen("tcp", ":9090")
+	if err != nil {
+		log.Error("unable to listen", "error", err)
+		os.Exit()
+	}
+	gs.Serve()
+
+}
